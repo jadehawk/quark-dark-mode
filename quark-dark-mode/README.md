93dbcf41876faac556cb5714596bc8495ba83645
Jadehawk <333657+jadehawk@users.noreply.github.com>
2023-11-02 05:23:02 -0400

    Initial Commit

---
+++ quark-dark-mode/README.md
@@ -0,0 +1,7 @@
+# Quark Dark Mode Theme
+
+The **Quark Dark Mode** Theme is for [Grav CMS](http://github.com/getgrav/grav).  This README.md file should be modified to describe the features, installation, configuration, and general usage of this theme.
+
+## Description
+
+Extending Quark Theme to Add Dark Mode Feature
---
+++ quark-dark-mode/quark-dark-mode.php
@@ -0,0 +1,35 @@
+<?php
+namespace Grav\Theme;
+
+use Grav\Common\Theme;
+
+class QuarkDarkMode extends Quark
+{
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onTwigSiteVariables' => ['onTwigSiteVariables', 0]
+        ];
+    }
+
+    /**
+     * Add theme assets globally
+     *
+     * @return void
+     */
+    public function onTwigSiteVariables()
+    {
+        // Add FontAwesome Globally
+        if ($this->config->get('theme.include_fontawesome')) {
+            $this->grav['assets']->add('theme://css/fontAwesome.css');
+        }
+
+        if ($this->config->get('theme.dark_mode')) {
+            $this->grav['assets']->add('theme://css/darkMode.css');
+            $this->grav['assets']->addJs('theme://js/darkMode.js', ['group' => 'bottom']);
+        }
+    }
+}
